02ad9e652cc79eac9f3726041425bba5ded3a1c8
Wojciech Strozynski <strozynskiw@gmail.com>
2021-12-14 17:41:52 +0100

    Cleanup for day 14

--- day_14/src/main.rs
+++ day_14/src/main.rs
@@ -1,11 +1,10 @@
-use std::collections::{HashMap};
+use std::collections::HashMap;
 use std::fs;
 use std::time::Instant;
 
 type Generated<'a> = (&'a str, HashMap<Vec<char>, char>);
 
 fn generate(input: &str) -> Generated {
-
     let mut parts = input.split("\n\n");
     let molecule = parts.next().unwrap();
     let rules = parts.next().unwrap();
@@ -14,14 +13,44 @@ fn generate(input: &str) -> Generated {
         .lines()
         .map(|l| l.trim())
         .filter(|l| !l.is_empty())
-        .map(|l| {let mut parts = l.split(" -> "); (parts.next().unwrap().chars().take(2).collect::<Vec<char>>(), parts.next().unwrap().chars().next().unwrap())})
+        .map(|l| {
+            let mut parts = l.split(" -> ");
+            (
+                parts.next().unwrap().chars().take(2).collect::<Vec<char>>(),
+                parts.next().unwrap().chars().next().unwrap(),
+            )
+        })
         .collect::<HashMap<Vec<char>, char>>();
 
     (molecule, rules)
 }
 
-fn synthetize(collection: &mut HashMap<char, usize>, subproducts: &HashMap<Vec<char>, usize>, rules: &HashMap<Vec<char>, char> ) -> HashMap<Vec<char>, usize> {
+fn initialize(
+    data: &Generated,
+    collection: &mut HashMap<char, usize>,
+    subproducts: &mut HashMap<Vec<char>, usize>,
+) {
+    // extract pairs and count initial number of elements
+    data.0
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(2)
+        .for_each(|w| {
+            *subproducts.entry(w.to_vec()).or_default() += 1;
+            *collection.entry(w[0]).or_default() += 1
+        });
 
+    // don't forget to the last character - not included above
+    *collection
+        .entry(data.0.chars().last().unwrap())
+        .or_default() += 1;
+}
+
+fn synthetize_step(
+    collection: &mut HashMap<char, usize>,
+    subproducts: &HashMap<Vec<char>, usize>,
+    rules: &HashMap<Vec<char>, char>,
+) -> HashMap<Vec<char>, usize> {
     let mut new_subproducts: HashMap<Vec<char>, usize> = HashMap::new();
 
     for sub in subproducts {
@@ -33,42 +62,31 @@ fn synthetize(collection: &mut HashMap<char, usize>, subproducts: &HashMap<Vec<c
         }
     }
 
-new_subproducts
+    new_subproducts
+}
+
+fn synthetize(data: &Generated, iterations: usize) -> usize {
+    let mut collection: HashMap<char, usize> = HashMap::new();
+    let mut subproducts: HashMap<Vec<char>, usize> = HashMap::new();
+
+    initialize(data, &mut collection, &mut subproducts);
+
+    for _ in 0..iterations {
+        subproducts = synthetize_step(&mut collection, &subproducts, &data.1);
+    }
+
+    let max = collection.values().max().unwrap();
+    let min = collection.values().min().unwrap();
+
+    max - min
 }
 
 fn part_1(input: &Generated) -> usize {
-    let mut collection: HashMap<char, usize> = HashMap::new();
-    let mut subproducts: HashMap<Vec<char>, usize> = HashMap::new();
-
-    input.0.chars().collect::<Vec<char>>().windows(2).for_each(|w| {*subproducts.entry(w.to_vec()).or_default() += 1; *collection.entry(w[0]).or_default() += 1 });
-    *collection.entry(input.0.chars().last().unwrap()).or_default() += 1;
-
-    for _ in 0..10 {
-        subproducts = synthetize(&mut collection, &subproducts, &input.1);
-    }
-
-    let max = collection.iter().map(|e| e.1).max().unwrap();
-    let min = collection.iter().map(|e| e.1).min().unwrap();
-
-    max - min
+    synthetize(input, 10)
 }
 
-
 fn part_2(input: &Generated) -> usize {
-    let mut collection: HashMap<char, usize> = HashMap::new();
-    let mut subproducts: HashMap<Vec<char>, usize> = HashMap::new();
-
-    input.0.chars().collect::<Vec<char>>().windows(2).for_each(|w| {*subproducts.entry(w.to_vec()).or_default() += 1; *collection.entry(w[0]).or_default() += 1 });
-    *collection.entry(input.0.chars().last().unwrap()).or_default() += 1;
-
-    for _ in 0..40 {
-        subproducts = synthetize(&mut collection, &subproducts, &input.1);
-    }
-
-    let max = collection.iter().map(|e| e.1).max().unwrap();
-    let min = collection.iter().map(|e| e.1).min().unwrap();
-
-    max - min
+    synthetize(input, 40)
 }
 
 fn main() {
@@ -84,18 +102,28 @@ fn main() {
     let res2 = part_2(&data);
     let res2_stop = Instant::now();
 
-    print!("Result1: {}\nResolved in: {:?}\n", res1, res1_stop.duration_since(res1_start));
-    print!("Result2: {}\nResolved in: {:?}\n", res2, res2_stop.duration_since(res2_start));
+    print!(
+        "Result1: {}\nResolved in: {:?}\n",
+        res1,
+        res1_stop.duration_since(res1_start)
+    );
+    print!(
+        "Result2: {}\nResolved in: {:?}\n",
+        res2,
+        res2_stop.duration_since(res2_start)
+    );
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-
     #[test]
     fn test_part_1() {
-        assert_eq!(1588, part_1(&generate("NNCB
+        assert_eq!(
+            1588,
+            part_1(&generate(
+                "NNCB
 
         CH -> B
         HH -> N
@@ -112,11 +140,16 @@ mod tests {
         BB -> N
         BC -> B
         CC -> N
-        CN -> C")));
+        CN -> C"
+            ))
+        );
     }
     #[test]
     fn test_part_2() {
-        assert_eq!(2188189693529, part_2(&generate("NNCB
+        assert_eq!(
+            2188189693529,
+            part_2(&generate(
+                "NNCB
 
         CH -> B
         HH -> N
@@ -133,6 +166,8 @@ mod tests {
         BB -> N
         BC -> B
         CC -> N
-        CN -> C")));
+        CN -> C"
+            ))
+        );
     }
 }
